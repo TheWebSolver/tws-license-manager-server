f2ab01c0f882b3742ea8e68756a5980c6d61196e
hsehszroc <thewebsolver@gmail.com>
2021-05-10 19:15:12 +0545

    ADDED: all singleton class instances. ADDED: container from tws-setting-framework. ADDED: dispatch of product metabox data to client.

--- Includes/Server.php
+++ Includes/Server.php
@@ -19,8 +19,15 @@
 
 namespace TheWebSolver\License_Manager;
 
+use LicenseManagerForWooCommerce\AdminMenus;
+use TheWebSolver\Core\Setting\Container;
 use TheWebSolver\License_Manager\API\S3;
+use TheWebSolver\License_Manager\Components\Checkout;
+use TheWebSolver\License_Manager\Components\Order;
 use TheWebSolver\License_Manager\Components\Product;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
 /**
  * TheWebSolver\License_Manager\Server class.
@@ -48,6 +55,27 @@ final class Server {
 	const PREFIX = 'tws_license_manager_server';
 
 	/**
+	 * Setting Container.
+	 *
+	 * @var Container
+	 */
+	public $container;
+
+	/**
+	 * TheWebSolver\License_Manager\Components\Checkout instance.
+	 *
+	 * @var Checkout
+	 */
+	public $checkout;
+
+	/**
+	 * TheWebSolver\License_Manager\Components\Order instance.
+	 *
+	 * @var Order
+	 */
+	public $order;
+
+	/**
 	 * Instantiates Server.
 	 *
 	 * @return Server
@@ -66,7 +94,45 @@ final class Server {
 	 * Private Constructor.
 	 */
 	private function __construct() {
-		$this->s3      = new S3();
-		$this->product = new Product();
+		$this->container = new Container( self::PREFIX, AdminMenus::LICENSES_PAGE );
+		$this->s3        = S3::load()->instance();
+		$this->product   = Product::load()->instance();
+		$this->checkout  = Checkout::load()->instance();
+		$this->order     = Order::load()->instance();
+
+		add_action( 'after_setup_theme', array( $this, 'add_admin_page' ) );
+
+		add_filter( 'hzfex_license_manager_server_pre_response_dispatch', array( $this, 'dispatch_product_details' ) );
+	}
+
+	/**
+	 * Adds WordPress admin page.
+	 */
+	public function add_admin_page() {
+		$this->container->set_page(
+			array(
+				'page_title' => __( 'Server Options', 'tws-license-manager-server' ),
+				'menu_title' => __( 'Server Setup', 'tws-license-manager-server' ),
+				'position'   => 99,
+			),
+		)
+		->set_capability( 'manage_options' )
+		->set_menu();
+	}
+
+	/**
+	 * Sends product meta details along with response.
+	 *
+	 * @param array $data The response data.
+	 *
+	 * @return array The modified response data.
+	 */
+	public function dispatch_product_details( array $data ) {
+		$meta = $this->product->get_data( $data['productId'] );
+
+		// Add meta as response data.
+		$data['meta'] = $meta;
+
+		return $data;
 	}
 }
