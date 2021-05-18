98f03015625503a510fda7b886f1e43714d0a62c
hsehszroc <thewebsolver@gmail.com>
2021-05-18 11:46:15 +0545

    ADDED: Amazon S3 full integration S3 hosts package & gets presigned URL for product updates on client. ADDED: option to disable S3 integration. ADDED: default option values. REVIEW: "Options_Interface" compatible codes. REVIEW: options placeholder and default values. REVIEW: other codes and comments.

--- Includes/API/S3.php
+++ Includes/API/S3.php
@@ -2,7 +2,7 @@
 /**
  * The Web Solver Licence Manager Server Amazon S3 Handler.
  *
- * @package TheWebSolver\License_Manager\Server\API
+ * @package TheWebSolver\License_Manager\Server\AWS
  *
  * -----------------------------------
  * DEVELOPED-MAINTAINED-SUPPPORTED BY
@@ -20,9 +20,14 @@
 namespace TheWebSolver\License_Manager\API;
 
 use Aws\S3\S3Client;
+use Aws\Credentials\Credentials;
+use Aws\Exception\AwsException;
+use Aws\S3\Exception\S3Exception;
+use LicenseManagerForWooCommerce\Models\Resources\License;
 use TheWebSolver\License_Manager\Server;
 use TheWebSolver\License_Manager\Options_Interface;
 use TheWebSolver\License_Manager\Single_Instance;
+use WP_Error;
 
 /**
  * TheWebSolver\License_Manager\API\S3 class.
@@ -52,11 +57,13 @@ class S3 implements Options_Interface {
 	 * @var array
 	 */
 	private $defaults = array(
-		's3_region'  => '',
-		's3_version' => '',
-		's3_key'     => '',
-		's3_secret'  => '',
-		's3_bucket'  => '',
+		'use_amazon_s3'     => 'on',
+		's3_region'         => '',
+		's3_version'        => 'latest',
+		's3_key'            => '',
+		's3_secret'         => '',
+		's3_bucket'         => '',
+		's3_url_expiration' => '10',
 	);
 
 	/**
@@ -72,24 +79,106 @@ class S3 implements Options_Interface {
 	 * @return S3
 	 */
 	public function instance() {
-		$this->options = wp_parse_args( (array) get_option( self::OPTION, array() ), $this->defaults );
+		$this->options = wp_parse_args( get_option( self::OPTION, array() ), $this->defaults );
 
 		return $this;
 	}
 
 	/**
-	 * Gets S3 Client instance.
+	 * Gets S3 Credentials.
 	 *
-	 * @return S3Client
+	 * @return Credentials|false False if option not set.
 	 */
-	public function get() {
-		$config = get_option( Server::PREFIX, array() );
-
-		if ( empty( $config ) ) {
-			return null;
+	private function get_credentials() {
+		if ( empty( $this->options['s3_key'] || $this->options['s3_secret'] ) ) {
+			return false;
 		}
 
-		return new S3Client( $config );
+		return new Credentials( $this->options['s3_key'], $this->options['s3_secret'] );
+	}
+
+	/**
+	 * Gets S3 Client Configuration.
+	 *
+	 * @return array|WP_Error Options not set properly, WP_Error happens.
+	 */
+	private function get_config() {
+		// Prepare config with Amazon S3 version.
+		$config      = array( 'version' => 'latest' );
+		$credentials = $this->get_credentials();
+
+		if ( empty( $this->options['s3_region'] ) ) {
+			return new WP_Error(
+				'amazon_s3_region_not_set',
+				__( 'Amazon S3 Region is not set.', 'tws-license-manager-server' ),
+				404
+			);
+		}
+
+		// Set Amazon S3 region from option.
+		$config['region'] = $this->options['s3_region'];
+
+		if ( false === $credentials ) {
+			return new WP_Error(
+				'amazon_s3_credentials_not_set',
+				__( 'Amazon S3 Credentials are not set.', 'tws-license-manager-server' ),
+				405
+			);
+		}
+
+		// Set Amazon S3 version from option.
+		if ( ! empty( $this->options['s3_version'] ) ) {
+			$config['version'] = $this->options['s3_version'];
+		}
+
+		// Set Amazon S3 credentials from option.
+		$config['credentials'] = $credentials;
+
+		return $config;
+	}
+
+	/**
+	 * Creates presigned URL for the package as download.
+	 *
+	 * @param License $license The current license instance.
+	 * @link https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/s3-presigned-url.html
+	 */
+	public function get_presigned_url_for( License $license ) {
+		$config = $this->get_config();
+		$meta   = Server::load()->product->get_data( $license->getProductId(), false );
+		$bucket = isset( $meta['bucket'] ) ? (string) $meta['bucket'] : $this->options['s3_bucket'];
+
+		if ( is_wp_error( $config ) ) {
+			return $config;
+		}
+
+		if ( ! isset( $meta['filename'] ) || empty( $meta['filename'] ) ) {
+			return new WP_Error(
+				'amazon_s3_key_not_set',
+				__( 'Amazon s3 Key/Product filename not set.', 'tws-license-manager-server' ),
+				406
+			);
+		}
+
+		try {
+			$client  = new S3Client( $config );
+			$cmd     = $client->getCommand(
+				'GetObject',
+				array(
+					'Bucket' => $bucket,
+					'Key'    => $meta['filename'],
+				)
+			);
+			$request = $client->createPresignedRequest( $cmd, "+{$this->options['s3_url_expiration']} Minutes" );
+		} catch ( S3Exception $e ) {
+			return new WP_Error( 'amazon_s3_exception', $e->getAwsErrorMessage(), $e->getAwsErrorCode() );
+		} catch ( AwsException $e ) {
+			return new WP_Error( 'amazon_aws_exception', $e->getAwsErrorMessage(), $e->getAwsErrorCode() );
+		} catch ( \InvalidArgumentException $e ) {
+			return new WP_Error( 'amazon_s3_args_invalid', $e->getMessage(), $e->getCode() );
+		}
+
+		return (string) $request->getUri();
 	}
 
 	/**
@@ -110,8 +199,8 @@ class S3 implements Options_Interface {
 	 *
 	 * @inheritDoc
 	 */
-	public function add_section() {
-		add_action( 'admin_init', array( $this, 'add_page_section' ), $this->option_priority );
+	public function add_page_section() {
+		add_action( 'admin_init', array( $this, 'add_section' ), $this->option_priority );
 	}
 
 	/**
@@ -119,7 +208,7 @@ class S3 implements Options_Interface {
 	 *
 	 * @inheritDoc
 	 */
-	public function add_page_section() {
+	public function add_section() {
 		Server::load()->container
 		->add_section(
 			self::OPTION,
@@ -132,6 +221,23 @@ class S3 implements Options_Interface {
 					'<a href="https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/s3-presigned-url.html" target="_blank">Amazon S3 Pre-Signed URL with AWS SDK for PHP Version 3
 					</a>'
 				),
+			)
+		)
+		->add_field(
+			'use_amazon_s3',
+			self::OPTION,
+			array(
+				'label'             => __( 'Use Amazon S3', 'tws-license-manager-server' ),
+				'desc'              => sprintf(
+					'%1$s <span class="option_notice alert">%2$s</span>',
+					__( 'Use Amazon S3 as storage for your premium plugins/themes and handle the plugin/theme update URL for the client.', 'tws-license-manager-server' ),
+					__( 'All options set below will be ignored when this is turned off.', 'tws-license-manager-server' ),
+				),
+				'type'              => 'checkbox',
+				'class'             => 'hz_switcher_control',
+				'sanitize_callback' => 'sanitize_key',
+				'priority'          => 1,
+				'default'           => 'on',
 			)
 		)
 		->add_field(
@@ -151,11 +257,13 @@ class S3 implements Options_Interface {
 			self::OPTION,
 			array(
 				'label'             => __( 'Amazon S3 Version', 'tws-license-manager-server' ),
-				'desc'              => __( 'Enter your Amazon S3 Version.', 'tws-license-manager-server' ),
+				'desc'              => __( 'Enter your Amazon S3 Version. Defaults to "latest"', 'tws-license-manager-server' ),
 				'type'              => 'text',
 				'sanitize_callback' => 'sanitize_text_field',
 				'class'             => 'widefat',
 				'priority'          => 10,
+				'placeholder'       => $this->defaults['s3_version'],
+				'default'           => $this->defaults['s3_version'],
 			)
 		)
 		->add_field(
@@ -163,7 +271,11 @@ class S3 implements Options_Interface {
 			self::OPTION,
 			array(
 				'label'             => __( 'Amazon S3 Bucket', 'tws-license-manager-server' ),
-				'desc'              => __( 'Enter your Amazon S3 Bucket Name. This will be your global bucket name. If you only use one bucket for all your products, then set it here and no need to enter it for each product. If you manage separate buckets, then enter them in product edit page.', 'tws-license-manager-server' ),
+				'desc'              => sprintf(
+					'%1$s <span class="option_notice success">%2$s</span>',
+					__( 'Enter your Amazon S3 Bucket Name. It will be your global bucket name.', 'tws-license-manager-server' ),
+					__( 'If you only use one bucket for all your products, then set it here, and no need to set it for each product. If you manage separate buckets, then set them on the product level.', 'tws-license-manager-server' )
+				),
 				'type'              => 'text',
 				'sanitize_callback' => 'sanitize_text_field',
 				'class'             => 'widefat',
@@ -193,6 +305,29 @@ class S3 implements Options_Interface {
 				'class'             => 'widefat hz_password_control',
 				'priority'          => 25,
 			)
+		)
+		->add_field(
+			's3_url_expiration',
+			self::OPTION,
+			array(
+				'label'             => __( 'Amazon S3 Pre-signed URL Expiration Time', 'tws-license-manager-server' ),
+				'desc'              => sprintf(
+					'%1$s <span class="option_notice alert">%2$s <a href="%3$s" target="_blank">%4$s</a></span>',
+					__( 'Enter the number of minutes after which the Pre-signed URL to the product package will expire. Expired links will no longer provide access to the package.', 'tws-license-manager-server' ),
+					__( 'The expiration link must be less than a week. i.e. "10080"', 'tws-license-manager-server' ),
+					'https://docs.aws.amazon.com/general/latest/gr/signature-version-4.html',
+					__( 'Learn more about it.', 'tws-license-manager-server' )
+				),
+				'type'              => 'number',
+				'sanitize_callback' => 'absint',
+				'class'             => 'widefat',
+				'priority'          => 30,
+				'placeholder'       => $this->defaults['s3_url_expiration'],
+				'default'           => $this->defaults['s3_url_expiration'],
+				'min'               => 1,
+				'max'               => 10080,
+				'step'              => 1,
+			)
 		);
 	}
 
@@ -201,7 +336,7 @@ class S3 implements Options_Interface {
 	 *
 	 * @inheritDoc
 	 */
-	public function get_options() {
+	public function get_option() {
 		return $this->options;
 	}
 }
